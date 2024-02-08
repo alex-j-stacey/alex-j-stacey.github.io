592627d35e6f2d604147da8ae0c6647c9f01e457
Alex Stacey <alexjstacey@gmail.com>
2024-02-07 19:58:09 -0600

    Added welcome div

--- site.js
+++ site.js
@@ -1,3 +1,13 @@
+const hours = new Date().getHours() // get the current hour
 
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+
+const welcome = document.querySelector('#welcome')
+if (isMorning) { welcome.textContent = 'Good Morning,' }
+else if (isAfternoon) { welcome.textContent = 'Good Afternoon,' }
+else {welcome.textContent = 'Good Evening,'}
 
 
